ea3854717d2a90ec1ddb23896e4235e70dd4bcb4
mollcano <MOLLYJBROOKS@GMAIL.COM>
2017-05-31 16:44:43 -0600

    full crud app on postman

--- main.go
+++ main.go
@@ -1,37 +1,84 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
-func items(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello items!")
+type Article struct {
+	ID      string `json:"id,omitempty"`
+	Title   string `json:"title, omitempty"`
+	Desc    string `json:"desc, omitempty"`
+	Content string `json:"content, omitempty"`
 }
-func newItem(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.NotFound(w, r)
+
+var articles []Article
+
+func GetArticlesEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for _, item := range articles {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
 	}
-	// handle the POST
-	//Call to ParseForm makes form fields available.
-	err := r.ParseForm()
-	if err != nil {
-		// Handle error here via logging and then return
-		http.Error(w, "form parse failed", http.StatusInternalServerError)
+	json.NewEncoder(w).Encode(&Article{})
+}
+
+func GetArticleEndpoint(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(articles)
+}
+
+func CreateArticleEndpoint(w http.ResponseWriter, req *http.Request) {
+	var article Article
+	_ = json.NewDecoder(req.Body).Decode(&article)
+	articles = append(articles, article)
+	json.NewEncoder(w).Encode(articles)
+}
+
+func DeleteArticleEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for index, item := range articles {
+		if item.ID == params["id"] {
+			articles = append(articles[:index], articles[index+1:]...)
+			break
+		}
 	}
-	name := r.PostFormValue("name")
-	fmt.Fprintf(w, "Hello, %s!", name)
+	json.NewEncoder(w).Encode(articles)
+}
+
+func UpdateArticleEndpoint(w http.ResponseWriter, req *http.Request) {
+
+	params := mux.Vars(req)
+	var article Article
+	for index, item := range articles {
+		if item.ID == params["id"] {
+			articles = append(articles[:index], articles[index+1:]...)
+			_ = json.NewDecoder(req.Body).Decode(&article)
+			articles = append(articles, article)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(articles)
 }
 
 func main() {
-	fmt.Println("hello World")
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/items", items)
-	http.HandleFunc("/new", newItem)
-	http.ListenAndServe(":8000", nil)
+	router := mux.NewRouter()
+	articles = append(articles, Article{ID: "1", Title: "Hello", Desc: "Description", Content: "Content"})
+	articles = append(articles, Article{ID: "2", Title: "Hello2", Desc: "Description", Content: "Content"})
+	router.HandleFunc("/articles", GetArticleEndpoint).Methods("GET")
+	router.HandleFunc("/articles/{id}", GetArticlesEndpoint).Methods("GET")
+	router.HandleFunc("/articles", CreateArticleEndpoint).Methods("POST")
+	router.HandleFunc("/articles/{id}", DeleteArticleEndpoint).Methods("DELETE")
+	router.HandleFunc("/articles/{id}", UpdateArticleEndpoint).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":12345", router))
 }
